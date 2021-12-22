1203cdc21961459e308765167c3367717c169435
Feyzullah YILDIZ <ahmet.feyzullah.yildiz@gmail.com>
2021-12-22 23:47:30 +0300

    header added

--- src/components/scss/header/index.tsx
+++ src/components/scss/header/index.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import styles from "./index.module.scss";
 
-import { Logo } from "@components/scss";
-
 export const Header: React.FC = () => {
   return (
     <div className={styles.header}>
-      <Logo />
+      <div className={`${styles.nav} container`}>
+        <h2>DEMO Streaming</h2>
+        <div className={styles.buttons}>
+          <a className={styles.login} target="_self" href="#">Log in</a>
+          <a className={styles.trialButton} href="#" target="_self">Start your free trial</a>
+        </div>
+      </div>
     </div>
   );
 };
